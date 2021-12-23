1ed342e4f0dee3d602b7c49bc3037bc29cc441d2
barskee <lukaszbarskee@gmail.com>
2021-12-23 18:22:30 +0100

    end of section 18

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,9 +18,7 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<IPhotoService, PhotoService>(); 
             services.AddScoped<ITokenService, TokenService>(); 
-            services.AddScoped<IUserRepository, UserRepository>(); 
-            services.AddScoped<ILikesRepository, LikesRepository>(); 
-            services.AddScoped<IMessageRepository, MessageRepository>(); 
+            services.AddScoped<IUnitOfWork, UnitOfWork>(); 
             services.AddScoped<LogUsersActivity>(); 
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly); 
             
